cb311851463d12ca8ba3bbdb9fe24a8bf70e931e
Nick Drozd <drozd@bitwise.io>
2018-06-13 10:55:37 -0500

    identity signer

--- sdk/examples/poet_python/sawtooth_poet_engine/oracle.py
+++ sdk/examples/poet_python/sawtooth_poet_engine/oracle.py
@@ -14,12 +14,17 @@
 # -----------------------------------------------------------------------------
 
 import logging
+import os
 
 from sawtooth_poet.poet_consensus.poet_block_publisher import PoetBlockPublisher
 from sawtooth_poet.poet_consensus.poet_block_verifier import PoetBlockVerifier
 from sawtooth_poet.poet_consensus.poet_fork_resolver import PoetForkResolver
 
 from sawtooth_sdk.consensus.exceptions import UnknownBlock
+
+import sawtooth_signing as signing
+from sawtooth_signing import CryptoFactory
+from sawtooth_signing.secp256k1 import Secp256k1PrivateKey
 
 
 LOGGER = logging.getLogger(__name__)
@@ -182,7 +187,10 @@ class _StateViewProxy:
 
 class _BatchPublisherProxy:
     def __init__(self):
-        self.identity_signer = _DummySigner()
+        # these should be passed in
+        key_dir, key_name = '/etc/sawtooth/keys', 'validator'
+
+        self.identity_signer = load_identity_signer(key_dir, key_name)
 
     def send(self, transactions):
         pass
@@ -191,3 +199,42 @@ class _BatchPublisherProxy:
 class _DummySigner:
     def sign(self, header):
         pass
+
+
+def load_identity_signer(key_dir, key_name):
+    """Loads a private key from the key directory, based on a validator's
+    identity.
+
+    Args:
+        key_dir (str): The path to the key directory.
+        key_name (str): The name of the key to load.
+
+    Returns:
+        Signer: the cryptographic signer for the key
+    """
+    key_path = os.path.join(key_dir, '{}.priv'.format(key_name))
+
+    if not os.path.exists(key_path):
+        raise Exception(
+            "No such signing key file: {}".format(key_path))
+    if not os.access(key_path, os.R_OK):
+        raise Exception(
+            "Key file is not readable: {}".format(key_path))
+
+    LOGGER.info('Loading signing key: %s', key_path)
+    try:
+        with open(key_path, 'r') as key_file:
+            private_key_str = key_file.read().strip()
+    except IOError as e:
+        raise Exception(
+            "Could not load key file: {}".format(str(e)))
+
+    try:
+        private_key = Secp256k1PrivateKey.from_hex(private_key_str)
+    except signing.ParseError as e:
+        raise Exception(
+            "Invalid key in file {}: {}".format(key_path, str(e)))
+
+    context = signing.create_context('secp256k1')
+    crypto_factory = CryptoFactory(context)
+    return crypto_factory.new_signer(private_key)
